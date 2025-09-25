40870aeb09f3cc865f24dd54b1ba261e1f9de23f
Fernando Martins <fernandom.adm@gmail.com>
2025-09-25 00:12:59 -0300

    Adicionando Token

---
+++ Autenticacao2Fatores_AcessoGit/src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.forum_hub.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmailIgnoreCase(String username);
+}
