e942240838abf918482622f7925afafc465c7b54
Fernando Martins <fernandom.adm@gmail.com>
2025-09-28 10:53:48 -0300

    Adicionando projeto Treinamento

---
+++ Treinamento Spring Security/Treinamento_Forun_hub/forun_hub/src/main/java/projeto_forun_hub/forun_hub/ForunHubApplication.java
@@ -0,0 +1,13 @@
+package projeto_forun_hub.forun_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForunHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForunHubApplication.class, args);
+	}
+
+}
---
+++ Treinamento Spring Security/Treinamento_Forun_hub/forun_hub/src/test/java/projeto_forun_hub/forun_hub/ForunHubApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_forun_hub.forun_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForunHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
